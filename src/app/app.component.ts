fdca77bbec038bc1a0ea1bf19a55deb137eefa3a
Venkatesh <venky.chowdary91@gmail.com>
2018-10-02 22:08:56 +0200

    Change titles and styling on homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { DomSanitizer} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'restraunt';
+
+/*   @ViewChild('videoPlayer') videoplayer: any;
+ */
+  constructor(private domSanitizer: DomSanitizer) {
+  }
+
+ /*  getVideoTag() {
+    return this.domSanitizer.bypassSecurityTrustHtml(
+      `<video loop muted autoplay>
+        <source src="../assets/video/background_video.mp4" type="video/mp4" />
+      </video>
+      <div class="container h-100">
+        <div class="d-flex text-center h-100">
+          <div class="my-auto w-100 text-white">
+            <h1 class="display-3">Namasthe</h1>
+            <h2>MITTAGSMENÜ alle gerichte (Abholung nur 5,90 / Lieferung nur 6,50)</h2>
+          </div>
+        </div>
+      </div>`
+    );
+  } */
 }
+
