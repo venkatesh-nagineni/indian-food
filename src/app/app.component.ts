f5c7af3e6a57fc4de2ff4521e6672492d6ffa158
Venkatesh <venky.chowdary91@gmail.com>
2018-10-03 14:36:11 +0200

    Change titles and styling on homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { DomSanitizer} from '@angular/platform-browser';
+import { Component, OnInit, ViewChild, AfterViewInit, HostListener } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 
 @Component({
@@ -7,27 +7,20 @@ import { DomSanitizer} from '@angular/platform-browser';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-/*   @ViewChild('videoPlayer') videoplayer: any;
- */
   constructor(private domSanitizer: DomSanitizer) {
+
   }
 
- /*  getVideoTag() {
-    return this.domSanitizer.bypassSecurityTrustHtml(
-      `<video loop muted autoplay>
-        <source src="../assets/video/background_video.mp4" type="video/mp4" />
-      </video>
-      <div class="container h-100">
-        <div class="d-flex text-center h-100">
-          <div class="my-auto w-100 text-white">
-            <h1 class="display-3">Namasthe</h1>
-            <h2>MITTAGSMENÜ alle gerichte (Abholung nur 5,90 / Lieferung nur 6,50)</h2>
-          </div>
-        </div>
-      </div>`
-    );
-  } */
+  ngOnInit() {
+    const mainNav = document.getElementById('js-menu');
+    const navBarToggle = document.getElementById('js-navbar-toggle');
+    navBarToggle.addEventListener('click', () => {
+      mainNav.classList.toggle('active');
+    });
+
+  }
+
 }
 
